faf09647edecd08a75ea2a39ca5f395dc1ffe6af
halo-eunsu <ujd017@naver.com>
2023-02-25 13:15:01 +0900

    수정 테스트

--- JES_needtohelp/team_project_01/ContentView.swift
+++ JES_needtohelp/team_project_01/ContentView.swift
@@ -10,28 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
     
-    ZStack(alignment: .bottomLeading) {
-        Color(red: 52 / 255, green: 152 / 255, blue: 219 / 255).ignoresSafeArea()
+
+      
         
-        Rectangle()
-            .fill(Color.red)
-            .frame(width: 100, height: 50)
-        
-        Image("background")
-            .resizable(resizingMode: .stretch)
-            .aspectRatio(contentMode: .fill)
+//        Rectangle()
+//            .fill(Color.red)
+//            .frame(width: 100, height: 50)
+//
+//        Image("background")
+//            .resizable(resizingMode: .stretch)
+//            .aspectRatio(contentMode: .fill)
             
         VStack{
                 NavigationView{
-                    
-                    NavigationLink(destination: next_page()) {
-                    
-                        Text("버튼입니다.")
-                            .frame(width: 100, height: 100)
+                    ZStack{
+//                        Color(red: 210 / 255, green: 255 / 255,                         blue: 255 / 255).ignoresSafeArea()
+//
+                        NavigationLink(destination: next_page()) {
+                            
+                            Text("버튼입니다.")
+                                .frame(width: 100, height: 100)
+                        }
                     }
                     
                 }
-        }
+        
             
     }
     }
